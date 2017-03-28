f0a189e7c042bdcc4793334cdb5d8f460a2bb243
bishtgeeta <geeta.bisht28@gmail.com>
2017-03-28 11:27:10 +0800

    changed np to numpy

--- main/AnalyseData.py
+++ main/AnalyseData.py
@@ -13,10 +13,10 @@ import myCythonFunc
 
 
 #########################################################
-inputDir = r'Z:\Geeta-Share\rod assembly\20170228-009(1)-output'
-fileName = r'Z:\Geeta-Share\rod assembly\20170228-009(1)-output\output\imgDataNM - Copy.dat'
+inputDir = r'Z:\Geeta-Share\rod assembly\20170228-006(1)-output'
+fileName = r'Z:\Geeta-Share\rod assembly\20170228-006(1)-output\output\imgDataNM - Copy.dat'
 outputDir = inputDir
-gImg = cv2.imread(r'Z:\Geeta-Share\rod assembly\20170228-009(1)-output\output\dataProcessing\gImgRawStack\000001.png',0)
+gImg = cv2.imread(r'Z:\Geeta-Share\rod assembly\20170228-006(1)-output\output\dataProcessing\gImgRawStack\000001.png',0)
 imgRow, imgCol = gImg.shape
 pixInNM = 1.15714713
 skiprows=0
